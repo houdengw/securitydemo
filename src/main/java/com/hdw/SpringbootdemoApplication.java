dd67990ffa2d4dc615af9e917c3bbb356a4e9c00
hou.dengwen <hou.dengwen@trs.com.cn>
2019-03-19 16:07:48 +0800

    修改项目名称

---
+++ src/main/java/com/hdw/SpringbootdemoApplication.java
@@ -0,0 +1,15 @@
+package com.hdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+
+	}
+
+}
